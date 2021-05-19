23e542af18ff7bd9a2754254163817e609aaa77e
emerson-argueta <emerson@emersonargueta.com>
2021-05-19 09:54:08 -0700

    updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     env: {
-        SPREADSHEET_ID: "1V6HSmfUp_feGMSaXHY67209OhfptYZTEpTmO0Af1jmQ",
-        GSHEET_CREDENTIALS: '{"installed":{"client_id":"310320347573-kj2ql7fc6c3n70nh0vuvpk1rkv63bhk7.apps.googleusercontent.com","project_id":"trustdonations-310415","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"OfgheORRKGKL0sqby4R_Vl9b","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}',
-        REDIS_URL: "rediss://:e56206a0efa446138d0d0c2dfb324420@us1-touched-walleye-32804.upstash.io:32804",
-        GOOGLE_ID: "310320347573-4eflgrc0004cir24lse1tsub9rijt4c3.apps.googleusercontent.com",
-        GOOGLE_SECRET: "vABlrDRC_f7zhIm3xGK46kJN",
         SITE: "https://www.emersonargueta.com"
     },
 }
